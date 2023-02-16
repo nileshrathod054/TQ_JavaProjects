22c494be0f66e85f085775be344d26d5e6940560
Nilesh Rathod <nileshrathod054@gmail.com>
2023-02-16 22:23:34 +0530

    commit

--- CollectionPrograms/src/mapEx/TreeMap1.java
+++ CollectionPrograms/src/mapEx/TreeMap1.java
@@ -1,6 +1,8 @@
 package mapEx;
 
 import java.util.HashMap;
+import java.util.NavigableMap;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class TreeMap1 {
@@ -10,12 +12,39 @@ public class TreeMap1 {
 		ts.put("red ballons", 12);
 		ts.put("black balloons", 34);
 		ts.put("yellow ballons", 2);
+		ts.put("blue ballons", 12);
+		ts.put("orange balloons", 34);
+		ts.put("white ballons", 2);
 
-		System.out.println(ts);
+		// System.out.println(ts);
 
 		TreeMap<String, Integer> ts1 = new TreeMap(ts);
 
 		System.out.println(ts1);
+		System.out.println("======");
+
+		SortedMap<String, Integer> sop = ts1.subMap("blue ballons", "white ballons");
+		System.out.println(sop);
+
+		System.out.println("------");
+		TreeMap<String, Integer> ts2 = new TreeMap<>(sop);
+		ts2.put("pink ballons", 20);
+		System.out.println(ts2);
+
+		System.out.println("-------ceiling entry---");
+
+		System.out.println(ts1.ceilingEntry("ped balloons"));
+
+		System.out.println(ts1.floorEntry("doll balloons"));
+
+		System.out.println(ts1.ceilingKey("doll ballons"));
+
+		System.out.println(ts1.lowerKey("red ballons"));
+
+		NavigableMap<String, Integer> ns = ts1.descendingMap();
+		System.out.println("---");
+
+		System.out.println(ns);
 
 	}
 
