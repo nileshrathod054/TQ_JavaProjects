f5cda585ab7c9a15f44e91406944bc817cda6106
Nilesh Rathod <nileshrathod054@gmail.com>
2023-04-13 09:36:37 +0530

    commit

---
+++ Mock/src/mock1/Operators.java
@@ -0,0 +1,18 @@
+package mock1;
+
+public class Operators {
+	public static void main(String[] args) {
+//		int val1 = 10;
+//		int val2 = 20;
+//
+//		int max = val1 >= val2 ? val1 : val2;
+//		System.out.println(max);
+//		boolean a=true;
+//		boolean b=false;
+//		System.out.println(a||b);
+
+		int a = 3, b = 8;
+		boolean c = a > 5 && ++b > 6;
+		System.out.println(b);
+	}
+}
