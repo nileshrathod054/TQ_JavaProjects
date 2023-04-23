dd33fe4d2bc8f810871e86eb6085190a76a5f753
Nilesh Rathod <nileshrathod054@gmail.com>
2023-04-23 10:46:05 +0530

    commit1

---
+++ TQ_JavaBatch/src/operators/Solution5.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class Solution5 {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		first: {
+			second: {
+				third: {
+					if (x == y >> 1) {
+						break second;
+					}
+				}
+				System.out.println(x);
+			}
+			System.out.println(y);
+		}
+	}
+}
